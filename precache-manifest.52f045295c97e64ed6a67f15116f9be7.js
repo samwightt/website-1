ea0229901851247dc9067b9a5f7e0a815582f6a8
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-01-25 16:07:33 +0000

    Deploy lablueprint/website to github.com/lablueprint/website.git:gh-pages

---
+++ precache-manifest.52f045295c97e64ed6a67f15116f9be7.js
@@ -0,0 +1,190 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08aececc9f1e256f81acd6d3f2d588a0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ed6b547f4e6218adafd2",
+    "url": "/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "421d29e3941625d46470",
+    "url": "/static/css/main.0b444ed3.chunk.css"
+  },
+  {
+    "revision": "ed6b547f4e6218adafd2",
+    "url": "/static/js/2.24352c40.chunk.js"
+  },
+  {
+    "revision": "63f134969c44b5050ed8625599c00247",
+    "url": "/static/js/2.24352c40.chunk.js.LICENSE"
+  },
+  {
+    "revision": "421d29e3941625d46470",
+    "url": "/static/js/main.09980038.chunk.js"
+  },
+  {
+    "revision": "a796a236ee8470d38315",
+    "url": "/static/js/runtime-main.783779e9.js"
+  },
+  {
+    "revision": "1a280523d375e9358d5229df34fc8e94",
+    "url": "/static/media/BPText-400.1a280523.woff2"
+  },
+  {
+    "revision": "46a7d48240d428c9dc3d4ff579199312",
+    "url": "/static/media/BPText-400.46a7d482.woff"
+  },
+  {
+    "revision": "528cb4a87fbf5acd1aa772752b6848b1",
+    "url": "/static/media/BPText-400.528cb4a8.eot"
+  },
+  {
+    "revision": "6eb275050498ab5692aff58bdd7a2643",
+    "url": "/static/media/BPText-400.6eb27505.ttf"
+  },
+  {
+    "revision": "a2ed588ba986f48bab85d75d51fd80fb",
+    "url": "/static/media/BPText-400.a2ed588b.svg"
+  },
+  {
+    "revision": "20cfc5c81c67573c98375b695e6a45c7",
+    "url": "/static/media/BPText-600.20cfc5c8.ttf"
+  },
+  {
+    "revision": "aa4405ed937295296cf8510f437628e0",
+    "url": "/static/media/BPText-600.aa4405ed.woff2"
+  },
+  {
+    "revision": "c2b154f61de0c3278da5cfb2a3440371",
+    "url": "/static/media/BPText-600.c2b154f6.svg"
+  },
+  {
+    "revision": "cf8e52f3dbc3bdaf571623e67df160a7",
+    "url": "/static/media/BPText-600.cf8e52f3.eot"
+  },
+  {
+    "revision": "e3bbab8d37ba508809f78c4baf02ebdb",
+    "url": "/static/media/BPText-600.e3bbab8d.woff"
+  },
+  {
+    "revision": "3bf73be5b5e225efc6d9d05696ff883b",
+    "url": "/static/media/HeaderText-400.3bf73be5.ttf"
+  },
+  {
+    "revision": "4b423707a8cc54f076bc0b34bc890780",
+    "url": "/static/media/HeaderText-400.4b423707.eot"
+  },
+  {
+    "revision": "993168eba0e293b90dabac2bc04639be",
+    "url": "/static/media/HeaderText-400.993168eb.svg"
+  },
+  {
+    "revision": "b0b3f17e6840538fe0c5309e2b96f329",
+    "url": "/static/media/HeaderText-400.b0b3f17e.woff"
+  },
+  {
+    "revision": "d7cfa042f25e95e221f73fa59e7d4a9d",
+    "url": "/static/media/HeaderText-400.d7cfa042.woff2"
+  },
+  {
+    "revision": "5666b1e1ae661e7972bc511060bce086",
+    "url": "/static/media/about.5666b1e1.jpg"
+  },
+  {
+    "revision": "8d766b0b59d0480f04f56e5bace04fcf",
+    "url": "/static/media/amazon.8d766b0b.png"
+  },
+  {
+    "revision": "8b4a057d0b3e9e84dbb4ddbd8fceb4bf",
+    "url": "/static/media/avantstay.8b4a057d.png"
+  },
+  {
+    "revision": "c13b18e212b0f05e46d62a9d641a2c81",
+    "url": "/static/media/cache_cds.c13b18e2.png"
+  },
+  {
+    "revision": "df5de19fdf6a8246a2155b8bede8f92f",
+    "url": "/static/media/couple.df5de19f.png"
+  },
+  {
+    "revision": "38c32feb555132e072faa6aad577d274",
+    "url": "/static/media/derek_chu.38c32feb.jpg"
+  },
+  {
+    "revision": "e2637a5fb6b462c07d6ea64b34fc4df3",
+    "url": "/static/media/green_dot.e2637a5f.png"
+  },
+  {
+    "revision": "82426847afd71e9e140416ceec8fa7fb",
+    "url": "/static/media/harmony.82426847.png"
+  },
+  {
+    "revision": "dbcc64b82545a18abc0da4d8d4291903",
+    "url": "/static/media/harmony.dbcc64b8.jpg"
+  },
+  {
+    "revision": "9f0af1d6616a6c0906c8fc061cd9dc41",
+    "url": "/static/media/home.9f0af1d6.png"
+  },
+  {
+    "revision": "9205cd269b616376e49ad165982211b1",
+    "url": "/static/media/ibm.9205cd26.png"
+  },
+  {
+    "revision": "e1df3c1fb329bac7a3ab23a4bea4c1d8",
+    "url": "/static/media/impact_hk.e1df3c1f.png"
+  },
+  {
+    "revision": "c5f79e2bad2356fa5ac31f3dd6d23f6a",
+    "url": "/static/media/innovation.c5f79e2b.png"
+  },
+  {
+    "revision": "a1892f9aaa193be0f5d3fb9919f36b86",
+    "url": "/static/media/justin_jeon.a1892f9a.jpg"
+  },
+  {
+    "revision": "4cc755a0e42fec51b7a996d4218eca44",
+    "url": "/static/media/leo_liu.4cc755a0.jpg"
+  },
+  {
+    "revision": "08a2ca02b425777ee6b0fe9d5ca8762e",
+    "url": "/static/media/ppg.08a2ca02.png"
+  },
+  {
+    "revision": "ec7e111d5f1b1f422ae788c6ba87c3a2",
+    "url": "/static/media/presentation.ec7e111d.png"
+  },
+  {
+    "revision": "1ae1fa8991efdf0135182f5f891bf26d",
+    "url": "/static/media/projects.1ae1fa89.jpg"
+  },
+  {
+    "revision": "aff5b3e4d69c203613ca091676143cb6",
+    "url": "/static/media/ptv.aff5b3e4.jpg"
+  },
+  {
+    "revision": "fa2c8eeef56057c6c58fa9fc440e918d",
+    "url": "/static/media/ptv.fa2c8eee.jpg"
+  },
+  {
+    "revision": "8cfcea6b05f4962f0a72d46216d81ee7",
+    "url": "/static/media/raymond_phan.8cfcea6b.jpg"
+  },
+  {
+    "revision": "14e67e240fd61327cd54fec70534410e",
+    "url": "/static/media/stripe.14e67e24.png"
+  },
+  {
+    "revision": "912ec0c40c869768425e4508978d0728",
+    "url": "/static/media/summer_siu.912ec0c4.jpg"
+  },
+  {
+    "revision": "f1c10706b99992f138776b6931f75c0c",
+    "url": "/static/media/tiffany_feng.f1c10706.jpg"
+  },
+  {
+    "revision": "d8fe396c09073db92a78238f417e8884",
+    "url": "/static/media/vincent_cheung.d8fe396c.jpg"
+  }
+]);
